0895df31b5fde656bf3bba830eed12093d003d4c
LeinadV <leinadv86@gmail.com>
2016-12-31 16:51:02 +0800

    change *.h to c*

--- ftp_proxy.cpp
+++ ftp_proxy.cpp
@@ -6,9 +6,9 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
 #include <netinet/in.h>
 #include <unistd.h>
 #include <errno.h>
